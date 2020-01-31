f56fd6832231ed20e3fe4fe729137a6e36da299e
jcLucio <c.lucio3@gmail.com>
2020-01-31 12:24:56 -0600

    Add loops to create a 16x16 div grid, and hover effect

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Javascript for PROJECT: ETCH-A-SKETCH
+
+const container = document.querySelector('#container');
+
+for (let i = 1; i <= 16; i++) {
+    const column = document.createElement('div');
+    container.appendChild(column);
+    column.classList.add('column')
+}
+
+for (let j = 1; j <= 16; j++) {
+    const columnList = document.querySelectorAll('.column');
+    columnList.forEach((col) => {
+        const row = document.createElement('div');
+        col.appendChild(row);
+        row.classList.add('row');
+        col.addEventListener('mouseover', (event) => {
+            event.target.style.background = 'blue';
+        });
+        col.addEventListener('mouseout', (event) => {
+            event.target.style.background = 'black';
+        });
+    });
+}
