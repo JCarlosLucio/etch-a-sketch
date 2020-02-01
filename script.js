9d91efc0bd5d249e26b6164f44dcd99dcd3ea544
jcLucio <c.lucio3@gmail.com>
2020-02-01 09:50:15 -0600

    Add createGrid, clearGrid, randomColor, paintRndColor, paintBlack functions, and add functionality to clear, size, and color buttons

--- script.js
+++ script.js
@@ -1,24 +1,88 @@
 // Javascript for PROJECT: ETCH-A-SKETCH
+const content = document.querySelector('#content');
+const sizeBtn = document.querySelector('#sizeBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const colorBtn = document.querySelector('#colorBtn');
+let gridSize = 16;
+let paintColor = 'black';
 
-const container = document.querySelector('#container');
-
-for (let i = 1; i <= 16; i++) {
-    const column = document.createElement('div');
-    container.appendChild(column);
-    column.classList.add('column')
+function createGrid(gridSize) {
+    for (let i = 1; i <= gridSize; i++) {
+        const column = document.createElement('div');
+        content.appendChild(column);
+        column.classList.add('column');
+    }
+    for (let j = 1; j <= gridSize; j++) {
+        let squareSize = 960 / gridSize;
+        const columnList = document.querySelectorAll('.column');
+        columnList.forEach((col) => {
+            const row = document.createElement('div');
+            col.appendChild(row);
+            row.classList.add('row');
+            row.style.width = `${squareSize}px`;
+            row.style.height = `${squareSize}px`;
+        });
+        if (paintColor === 'black') {
+            paintBlack();
+        } else {
+            paintRndColor();
+        }
+    }
 }
-
-for (let j = 1; j <= 16; j++) {
+function clearGrid(gridSize) {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+}
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+function paintBlack() {
     const columnList = document.querySelectorAll('.column');
     columnList.forEach((col) => {
-        const row = document.createElement('div');
-        col.appendChild(row);
-        row.classList.add('row');
         col.addEventListener('mouseover', (event) => {
-            event.target.style.background = 'blue';
-        });
-        col.addEventListener('mouseout', (event) => {
             event.target.style.background = 'black';
         });
     });
+    colorBtn.textContent = 'Colors'
 }
+function paintRndColor() {
+    const columnList = document.querySelectorAll('.column');
+    columnList.forEach((col) => {
+        col.addEventListener('mouseover', (event) => {
+            event.target.style.background = `${randomColor()}`;
+        });
+    });
+    colorBtn.textContent = 'Black'
+}
+
+sizeBtn.addEventListener('click', (e) => {
+    clearGrid(gridSize);
+    gridSize = parseInt(prompt('Enter grid size number(i.e. 32 for a 32x32 grid)'), '0');
+    if (gridSize < 0) {
+        gridSize = 16;
+    } else if (isNaN(gridSize)){
+        gridSize = 16;
+    }else if (gridSize >64){
+        gridSize = 64;
+    }
+    createGrid(gridSize);
+})
+clearBtn.addEventListener('click', (e) => {
+    clearGrid(gridSize);
+    createGrid(gridSize);
+})
+colorBtn.addEventListener('click', (e) => {
+    if (paintColor === 'black') {
+        paintRndColor();
+        paintColor = 'random';
+    } else {
+        paintBlack();
+        paintColor = 'black';
+    }
+})
+
+createGrid(gridSize);
